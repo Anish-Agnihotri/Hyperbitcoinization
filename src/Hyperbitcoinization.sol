5cc1f24802d92b211952ee7c52e85b8568c9ef44
Anish Agnihotri <contact@anishagnihotri.com>
2023-03-17 17:36:17 -0400

    wip: settlement

--- src/Hyperbitcoinization.sol
+++ src/Hyperbitcoinization.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 /// ============ Imports ============
 
 import "./interfaces/IERC20.sol"; // ERC20 minified interface
+import "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; // Chainlink pricefeed
 
 /// @title Hyperbitcoinization
 /// @author Anish Agnihotri
@@ -14,6 +15,8 @@ contract Hyperbitcoinization {
 
     /// @notice Bet terms
     struct Bet {
+        /// @notice Completed bet
+        bool completed;
         /// @notice USDC-providing party
         address partyUSDC;
         /// @notice wBTC-providing party
@@ -32,6 +35,8 @@ contract Hyperbitcoinization {
     IERC20 public immutable USDC_TOKEN;
     /// @notice WBTC token
     IERC20 public immutable WBTC_TOKEN;
+    /// @notice BTC/USD price feed (Chainlink)
+    AggregatorV3Interface public immutable BTCUSD_PRICEFEED;
 
     /// ============ Mutable storage ============
 
@@ -45,9 +50,11 @@ contract Hyperbitcoinization {
     /// @notice Creates a new Hyperbitcoinization contract
     /// @param _USDC_TOKEN address of USDC token
     /// @param _WBTC_TOKEN address of WBTC token
-    constructor(address _USDC_TOKEN, address _WBTC_TOKEN) {
+    /// @param _WBTC_PRICEFEED address of pricefeed for BTC/USD
+    constructor(address _USDC_TOKEN, address _WBTC_TOKEN, address _BTCUSD_PRICEFEED) {
         USDC_TOKEN = IERC20(_USDC_TOKEN);
         WBTC_TOKEN = IERC20(_WBTC_TOKEN);
+        BTCUSD_PRICEFEED = AggregatorV3Interface(_BTCUSD_PRICEFEED);
     }
 
     /// ============ Functions ============
@@ -76,17 +83,16 @@ contract Hyperbitcoinization {
         require(msg.sender == bet.partyUSDC, "Not USDC sending party");
 
         // Transfer USDC
-        bool success = USDC_TOKEN.transferFrom(
+        USDC_TOKEN.transferFrom(
             msg.sender,
             address(this),
             1_000_000e6
         );
-        require(success, "Failed collecting USDC");
 
         // Toggle partyASent
         bet.USDCSent = true;
 
-        if (bet.USDCSent && bet.WBTCSent) {
+        if (bet.WBTCSent) {
             bet.startTimestamp = block.timestamp;
         }
     }
@@ -100,22 +106,44 @@ contract Hyperbitcoinization {
         require(msg.sender == bet.partyWBTC, "Not wBTC sending party");
 
         // Transfer WBTC
-        bool success = WBTC_TOKEN.transferFrom(
+        WBTC_TOKEN.transferFrom(
             msg.sender,
             address(this),
             1e8
         );
-        require(success, "Failed collecting wBTC");
 
         // Toggle partyBSent
         bet.WBTCSent = true;
 
-        if (bet.USDCSent && bet.WBTCSent) {
+        if (bet.USDCSent) {
             bet.startTimestamp = block.timestamp;
         }
     }
 
-    function withdrawStale(uint256 betId) external {}
+    function settleBet(uint256 betId) external {
+        Bet memory bet = bets[betId];
+        require(!bet.completed, "Prevent settling completed bet");
+        require(bet.startTimestamp + 7776000 >= block.timestamp, "Bet still pending");
 
-    function determineWinner() external {}
+        // Mark bet completed
+        bet.completed = true;
+
+        // Collect BTC price
+        (,int price,,) = BTCUSD_PRICEFEED.latestRoundData();
+        uint256 wBTCPrice = uint256(price) / 10 ** BTCUSD_PRICEFEED.decimals();
+
+        address winner;
+        if (wBTCPrice > 1e6) {
+            winner = bet.partyWBTC;
+        } else {
+            winner = bet.partyUSDC;
+        }
+
+        USDC.transferFrom(address(this), winner, 1_000_000e6);
+        WBTC.transferFrom(address(this), winner, 1e8);
+    }
+
+    function withdrawStale(uint256 betId) external {
+
+    }
 }
